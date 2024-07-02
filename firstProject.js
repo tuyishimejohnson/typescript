3e66a62a26a35cded62892c65629e71c8d45ac36
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-02 21:58:34 +0200

    Creating functions in TypeScript

---
+++ firstProject.js
@@ -0,0 +1,21 @@
+var x = "Hello Johnson";
+var p = [20, 30];
+var a = p[0], b = p[1];
+console.log(p);
+console.log(x);
+var w = 4;
+console.log(w);
+// Creating an object
+var obj = { name: "Johnson", age: 34 };
+console.log(obj);
+// Union types
+function myParameters(para) {
+    console.log("This is the number ".concat(para));
+}
+myParameters(2);
+// Creating functions in TS
+function multiply(n) {
+    return n * 2;
+}
+var ml = multiply(3);
+console.log(ml);
--- firstProject.ts
+++ firstProject.ts
@@ -1,2 +1,33 @@
-let firstname: string = "JOhnson"
-console.log(firstname)
+const x: string = "Hello Johnson"
+const p: [number, number] = [20, 30]
+const [a, b] = p
+console.log(p)
+console.log(x)
+
+let w : number = 4
+console.log(w)
+
+// Creating an object
+
+const obj: {name: string, age: number} = {name: "Johnson", age: 34}
+console.log(obj)
+
+
+// Union types
+
+function myParameters(para: number | string) {
+    console.log(`This is the number ${para}`)
+}
+
+myParameters(2)
+
+
+// Creating functions in TS
+
+function multiply(n:number){
+    return n * 2
+}
+
+
+let ml = multiply(3) 
+console.log(ml)
